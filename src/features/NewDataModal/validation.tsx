2123a2bb42fdd4477f4b7eb3312d82337c932298
AlexanderKovalevCatena <alexnader.kovalev@catenamedia.com>
2024-05-20 18:42:38 +0200

    Fixed validation

--- src/features/NewDataModal/validation.tsx
+++ src/features/NewDataModal/validation.tsx
@@ -9,7 +9,7 @@ export const newDataValidationSchema = z.object({
   type: z.string().min(1, REQUIRED_FIELD_ERROR).max(256),
   companies: z.string(),
   description: z.string(),
-  scope: z.string().min(1, REQUIRED_FIELD_ERROR).max(1028),
+  scope: z.string().max(1028),
   expert: z.array(z.string()).min(1, REQUIRED_FIELD_ERROR),
 }).superRefine((schema, ctx) => {
   if (schema.type === EProjectType.COMPANY_RESEARCH) {
